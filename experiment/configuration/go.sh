bd354c370411f522de883b6bab53c4e9e8f14505
Kor de Jong <k.dejong1@uu.nl>
2023-01-19 11:32:49 +0100

    Port to eejit

--- experiment/configuration/go.sh
+++ experiment/configuration/go.sh
@@ -5,6 +5,12 @@ lue_install_prefix=$LUE_OBJECTS
 export PATH="$lue_install_prefix/bin:$PATH"
 
 hostname=$(hostname -s)
+
+if [[ $hostname == "login01" ]];
+then
+    hostname="eejit"
+fi
+
 command="hello_world.py"
 
 
@@ -18,11 +24,21 @@ then
     export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4"
     result_prefix="/mnt/data2/kor/data/lue_qa"
     worker="thread_numa_node"
+elif [[ $hostname == "eejit" ]];
+then
+    export PYTHONPATH="$lue_install_prefix/lib/python3.9:$PYTHONPATH"
+    # TODO Setting this loads the wrong libz ...
+    # export LD_PRELOAD="$GOOGLE_PERFTOOLS_ROOT/lib/libtcmalloc_minimal.so.4"
+    result_prefix="$HOME/development/data/lue_qa"
+    worker="cluster_node"
+    worker="numa_node"
+    worker="thread_cluster_node"
+    worker="thread_numa_node"
 elif [[ $hostname == "snowdon" ]];
 then
     export PYTHONPATH="$lue_install_prefix/lib/python3.10:$PYTHONPATH"
     export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4"
-    result_prefix="/home/kor/development/data/lue_qa"
+    result_prefix="$HOME/development/data/lue_qa"
     worker="thread_numa_node"
 elif [[ $hostname == "velocity" ]];
 then
@@ -39,8 +55,8 @@ config_prefix="$lue_qa/configuration/$hostname"
 sm_args="--cores 1 --configfile "$config_prefix/config.yaml" -- "
 
 
-# experiment="partition_shape"
-# output_directory=$result_prefix/${hostname}/${command}/${worker}/${experiment}
+experiment="partition_shape"
+output_directory="$result_prefix/${hostname}/${command}/${worker}/${experiment}"
 # if [ -d $output_directory ]; then
 #     mv $output_directory "$output_directory-`stat -c %y ${output_directory}`"
 # fi
@@ -50,8 +66,8 @@ sm_args="--cores 1 --configfile "$config_prefix/config.yaml" -- "
 # snakemake ${sm_args} $output_directory/postprocess.done
 
 
-# experiment="strong_scalability"
-# output_directory=$result_prefix/${hostname}/${command}/${worker}/${experiment}
+experiment="strong_scalability"
+output_directory=$result_prefix/${hostname}/${command}/${worker}/${experiment}
 # if [ -d $output_directory ]; then
 #     mv $output_directory "$output_directory-`stat -c %y ${output_directory}`"
 # fi
@@ -61,8 +77,8 @@ sm_args="--cores 1 --configfile "$config_prefix/config.yaml" -- "
 # snakemake ${sm_args} $output_directory/postprocess.done
 
 
-# experiment="weak_scalability"
-# output_directory=$result_prefix/${hostname}/${command}/${worker}/${experiment}
+experiment="weak_scalability"
+output_directory=$result_prefix/${hostname}/${command}/${worker}/${experiment}
 # if [ -d $output_directory ]; then
 #     mv $output_directory "$output_directory-`stat -c %y ${output_directory}`"
 # fi
